8c77c8581dcb99b219b1064a31f96f201d9228a3
mehari <gmehari.edu@gmail.com>
2018-12-21 16:21:58 +0300

    :star: new:add init models

--- server/boot/seed.js
+++ server/boot/seed.js
@@ -1,5 +1,32 @@
 const { error } = require("../../common/util");
 
+async function seedCategories(app) {
+  const { PostCategory } = app.models;
+  const categories = [
+    { name: "Infrastructure" },
+    { name: "Health" },
+    { name: "Other" }
+  ];
+  for (const category of categories) {
+    // eslint-disable-next-line
+    await PostCategory.findOrCreate({ name: category.name }, category);
+  }
+}
+async function seedTags(app) {
+  const { Tag } = app.models;
+  const tags = [
+    { name: "Internet" },
+    { name: "Social Media" },
+    { name: "HIV" },
+    { name: "Ebola" },
+    { name: "Children" }
+  ];
+  for (const tag of tags) {
+    // eslint-disable-next-line
+    await Tag.findOrCreate({ name: tag.name }, tag);
+  }
+}
+
 async function seedRoles(app) {
   const { UserRole } = app.models;
   const roles = [{ name: "admin" }, { name: "member" }];
@@ -36,5 +63,7 @@ module.exports = async function(app) {
   app.logger.info("Seeding started");
   await seedRoles(app);
   await seedAdmins(app);
+  await seedCategories(app);
+  await seedTags(app);
   app.logger.info("Seeding complete");
 };
