85e7627841f39e2a008856bb5654eb9e0364f66f
mehari <gmehari.edu@gmail.com>
2018-12-24 10:05:54 +0300

    new: validate required relations

--- common/models/feedback-replay.js
+++ common/models/feedback-replay.js
@@ -1,1 +1,11 @@
-module.exports = function() {};
+module.exports = function(FeedbackReplay) {
+  FeedbackReplay.validatesPresenceOf("createdById", {
+    message: "createdById is required"
+  });
+  FeedbackReplay.validatesPresenceOf("postId", {
+    message: "postId is required"
+  });
+  FeedbackReplay.validatesPresenceOf("feedbackId", {
+    message: "feedbackId is required"
+  });
+};
--- common/models/feedback.js
+++ common/models/feedback.js
@@ -1,1 +1,6 @@
-module.exports = function() {};
+module.exports = function(Feedback) {
+  Feedback.validatesPresenceOf("createdById", {
+    message: "createdById is required"
+  });
+  Feedback.validatesPresenceOf("postId", { message: "postId is required" });
+};
