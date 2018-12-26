3adf83c7805f0eab808283da0f51cbc27ca86f88
tsadkan <findtsadik@gmail.com>
2018-12-26 18:12:35 +0300

    new: added update category endpoint for post category

--- common/models/post-category.js
+++ common/models/post-category.js
@@ -1,1 +1,47 @@
-module.exports = function() {};
+const {
+  error,
+  validatesAbsenceOf,
+  validateRequiredFields
+} = require("../util");
+
+module.exports = function(PostCategory) {
+  /**
+   * update post category
+   */
+  PostCategory.updateCategory = async (accessToken, body) => {
+    const fields = ["id", "name", "color"];
+    const requiredFields = ["id"];
+
+    if (!accessToken || !accessToken.userId) throw error("Forbidden User", 403);
+
+    validatesAbsenceOf(fields, body);
+    validateRequiredFields(requiredFields, body);
+
+    const category = await PostCategory.findById(body.id);
+    delete category.id;
+
+    await category.patchAttributes({ ...body });
+
+    return { status: true };
+  };
+  PostCategory.remoteMethod("updateCategory", {
+    description: "update category",
+    accepts: [
+      {
+        arg: "accessToken",
+        type: "object",
+        http: ctx => {
+          const req = ctx && ctx.req;
+          const accessToken = req && req.accessToken;
+          return accessToken ? req.accessToken : null;
+        }
+      },
+      { arg: "body", type: "object", required: true }
+    ],
+    returns: {
+      type: "object",
+      root: true
+    },
+    http: { verb: "patch", path: "/update-category" }
+  });
+};
