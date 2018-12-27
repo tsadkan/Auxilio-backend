c233a88405a2ec0045267700e9cd43334257401f
mehari <gmehari.edu@gmail.com>
2018-12-27 16:45:53 +0300

    :bug: fix util methods this binding issue

--- common/util/index.js
+++ common/util/index.js
@@ -1,11 +1,11 @@
-module.exports = {
-  error: (message, code) => {
-    const e = new Error(message || "Internal Server Error!");
-    e.status = code || 500;
-    e.expose = true;
-    return e;
-  },
+const error = (message, code) => {
+  const e = new Error(message || "Internal Server Error!");
+  e.status = code || 500;
+  e.expose = true;
+  return e;
+};
 
+module.exports = {
   /**
    * Check if a data is owned by userId
    * Note:
@@ -24,14 +24,14 @@ module.exports = {
   validateRequiredFields(fields, data) {
     for (let i = 0, l = fields.length; i < l; i += 1) {
       if (Object.keys(data).indexOf(fields[i]) === -1) {
-        this.error(`property ${fields} is required`, 422);
+        error(`property ${fields} is required`, 422);
       }
     }
   },
   validatesAbsenceOf(fields, data) {
     for (const key in data) {
       if (fields.indexOf(key) === -1) {
-        this.error(`property ${key} doesn't exist.`, 422);
+        error(`property ${key} doesn't exist.`, 422);
       }
     }
   },
